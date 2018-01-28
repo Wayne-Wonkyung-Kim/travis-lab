283ff916136e7bd3ca9bc8982b96d8cb2ceb4959
wk2294 <wk2294@columbia.edu>
2018-01-28 16:48:00 -0500

    Corrected format

---
+++ test3.py
@@ -0,0 +1,20 @@
+import io
+
+def getCharNum( filename ):
+    input = io.open( filename, "r" )
+    count = 0
+
+    x = input.readline()
+    while x:
+        x = x.strip()
+        count += len( x )
+        x = input.readline()
+
+    return count
+
+
+def test_getCharNum():
+    assert getCharNum( "input.txt" ) == 6
+
+
+
