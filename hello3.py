b7fb135e04fbfd9c97b8f6cf43bcafd9e1b98ded
wk2294 <wk2294@columbia.edu>
2018-01-28 16:36:04 -0500

    finally

---
+++ hello3.py
@@ -0,0 +1,20 @@
+
+
+def getCharNum( filename ):
+    input = open( filename, "r" )
+    count = 0
+
+    x = input.readline()
+    while x:
+        x = x.strip()
+        count += len( x )
+        x = input.readline()
+
+    return count
+
+
+def test_getCharNum():
+    assert getCharNum( "input.txt" ) == 6
+
+
+
